faa0af9270adc206d6286179f6bb94e225259ab0
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-05-22 11:21:44 -0300

    add mutation example usage

--- src/__mocks__/mock-user-settings.ts
+++ src/__mocks__/mock-user-settings.ts
@@ -1,12 +1,10 @@
 export const mockUserSettings = [
   {
-    id: 1,
     userId: 1,
     receiveNotifications: false,
     receiveEmails: false,
   },
   {
-    id: 4,
     userId: 4,
     receiveNotifications: true,
     receiveEmails: false,
--- src/graphql/resolvers/user.resolver.ts
+++ src/graphql/resolvers/user.resolver.ts
@@ -5,12 +5,15 @@ import {
   Int,
   ResolveField,
   Parent,
+  Mutation,
 } from '@nestjs/graphql';
 import { User } from '../models/user';
 import { mockUsers } from '../../__mocks__/mock-users';
 import { NotFoundException } from '@nestjs/common';
 import { UserSetting } from '../models/user-setting';
 import { mockUserSettings } from '../../__mocks__/mock-user-settings';
+import { CreateUserInput } from './create-user.input';
+import { AddSettingsInput } from './add-settings.input';
 
 @Resolver(() => User)
 export class UserResolver {
@@ -29,5 +32,25 @@ export class UserResolver {
   @ResolveField(() => UserSetting)
   settings(@Parent() user: User): UserSetting {
     return mockUserSettings.find((setting) => setting.userId === user.id);
+  }
+
+  @Mutation(() => User)
+  createUser(@Args('input') input: CreateUserInput): User {
+    const newUser = { id: this.getNextId(), ...input };
+    mockUsers.push(newUser);
+    return newUser;
+  }
+
+  @Mutation(() => UserSetting)
+  addSettings(@Args('input') input: AddSettingsInput): UserSetting {
+    this.getUserById(input.userId);
+    mockUserSettings.push(input);
+    return input;
+  }
+
+  private getNextId() {
+    return mockUsers.length > 0
+      ? Math.max(...mockUsers.map((user) => user.id)) + 1
+      : 1;
   }
 }
